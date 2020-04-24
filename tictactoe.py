7c6da06eb57535d4a4c53685035b23aed6b9a563
SyedaAtiyaHusain <53337550+SyedaAtiyaHusain@users.noreply.github.com>
2020-04-24 23:26:27 +0530

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -2,6 +2,7 @@
 Developed By: Syeda Atiya Husain
 Requirement: Python version 3 or later
 """
+
 from functions import *                                                     #importing necessary functions present in functions.py module.
 play=1    
 while(play):        
@@ -24,9 +25,10 @@ while(play):
                 p1=input("Turn of player1:-")
                 turn_count+=1
                 turn=0
-                s1,s2,s3,s4,s5,s6,s7,s8,s9=markup(s1,s2,s3,s4,s5,s6,s7,s8,s9,p1,m1) #to place the markup in                                                                                             the board
+                s1,s2,s3,s4,s5,s6,s7,s8,s9=markup(s1,s2,s3,s4,s5,s6,s7,s8,s9,p1,m1) #to place the markup in the board
                 print(pattern.format(s1,s2,s3,s4,s5,s6,s7,s8,s9))                         
-                if(check_win(s1,s2,s3,s4,s5,s6,s7,s8,s9)):                  #to check does the player1 won the                                                                                   match after his turn or not
+                
+                if(check_win(s1,s2,s3,s4,s5,s6,s7,s8,s9)):                  #to check does the player1 won the match after his turn or not
                     print("Player1 has Won the game")
                     break
                 elif turn_count==9:                                         #to check tie in game
@@ -36,15 +38,15 @@ while(play):
                 p2=input("Turn of player2:-")                               #if it is turn of player 2
                 turn=1
                 turn_count+=1
-                s1,s2,s3,s4,s5,s6,s7,s8,s9=markup(s1,s2,s3,s4,s5,s6,s7,s8,s9,p2,m2) #to place the markup in                                                                                            the board
+                s1,s2,s3,s4,s5,s6,s7,s8,s9=markup(s1,s2,s3,s4,s5,s6,s7,s8,s9,p2,m2) #to place the markup in the board
                 print(pattern.format(s1,s2,s3,s4,s5,s6,s7,s8,s9))
-                if(check_win(s1,s2,s3,s4,s5,s6,s7,s8,s9)):                  #to check does the player1 won the                                                                                  match after his turn or not
+                if(check_win(s1,s2,s3,s4,s5,s6,s7,s8,s9)):                  #to check does the player1 won the match after his turn or not
                     print("Player2 has Won the game!!!!")
                     break
                 elif turn_count==9:                                         #to check tie in game
                     print("Tie!!!")
                     break
-        play=int(input("Want to play again?\nEnter 1 to start:-\nEnter 0 to quit:-")) #to ask if the players want                                                                                          to play again
+        play=int(input("Want to play again?\nEnter 1 to start:-\nEnter 0 to quit:-")) #to ask if the players want to play again
     else:
         print("Game closed successfully!!!!")                                      
         play=0                                                              #to stop the game
